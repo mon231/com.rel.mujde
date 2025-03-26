235e4026253db65d3308e906c2d3865ad5a21eff
Ariel Tubul <avarti765@gmail.com>
2025-03-26 19:02:10 +0200

    ensured ability to execute foreground service, with root subprocesses

--- client/app/src/main/java/com/rel/mujde/FridaInjectorService.java
+++ client/app/src/main/java/com/rel/mujde/FridaInjectorService.java
@@ -5,11 +5,19 @@ import android.util.Log;
 import android.os.IBinder;
 import android.app.Service;
 import android.content.Intent;
+import android.content.Context;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 public class FridaInjectorService extends Service {
     private Thread injectorThread = null;
     private boolean shouldContinueLooping = false;
+    private Notification serviceNotificaton = null;
+    final private String CHANNEL_ID = "FrideInjectorChannel";
 
     @Nullable
     @Override
@@ -18,13 +26,41 @@ public class FridaInjectorService extends Service {
     }
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // foreground services must create notification-channel (for Android 8+)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                CHANNEL_ID, CHANNEL_ID, NotificationManager.IMPORTANCE_DEFAULT
+            );
+
+            NotificationManager manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.createNotificationChannel(channel);
+        }
+    }
+
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         shouldContinueLooping = true;
 
-        if (injectorThread == null || !injectorThread.isAlive()) {
-            injectorThread = new Thread(() -> injectorThreadLogic());
-            injectorThread.start();
+        if (injectorThread != null && injectorThread.isAlive()) {
+            return START_STICKY;
         }
+
+        if (serviceNotificaton == null) {
+            serviceNotificaton = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Mujde Injector Service")
+                .setContentText("Mujde injector is running")
+                .setSmallIcon(android.R.drawable.ic_notification_overlay)
+                .build();
+
+            final int SERVICE_ID = 845;
+            startForeground(SERVICE_ID, serviceNotificaton);
+        }
+
+        injectorThread = new Thread(() -> injectorThreadLogic());
+        injectorThread.start();
 
         return START_STICKY;
     }
@@ -37,6 +73,9 @@ public class FridaInjectorService extends Service {
         if (injectorThread != null) {
             try {
                 injectorThread.wait();
+
+                injectorThread = null;
+                serviceNotificaton = null;
             } catch (Exception e) {
                 Log.e("[Mujde]", "Error waiting for thread: " + e.getMessage());
             }
