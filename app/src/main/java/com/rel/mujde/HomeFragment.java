2f124e6abd33820bde96ee2054fbefc9741a1cbd
Ariel Tubul <avarti765@gmail.com>
2025-04-04 17:35:45 +0300

    removed unused imports

--- app/src/main/java/com/rel/mujde/HomeFragment.java
+++ app/src/main/java/com/rel/mujde/HomeFragment.java
@@ -1,27 +1,13 @@
 package com.rel.mujde;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
-
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class HomeFragment extends Fragment {
     public HomeFragment() { }
